aaf14d5ed0927c03aa4d3338d980736e31765416
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-04-25 17:04:17 +0530

    create method linear

--- src/linear/LinearSearch.java
+++ src/linear/LinearSearch.java
@@ -1,4 +1,12 @@
 package linear;
 
 public class LinearSearch {
+    public int linear(int[] array, int target) {
+        for (int i : array) {
+            if (target == i) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
