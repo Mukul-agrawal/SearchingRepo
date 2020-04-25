4ddc1072888de481b5aa8a21e71a04b7451daf44
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-04-25 16:57:55 +0530

    create noOfElements to take input

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,5 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements : ");
+        int noOfElements = scanner.nextInt();
     }
 }
