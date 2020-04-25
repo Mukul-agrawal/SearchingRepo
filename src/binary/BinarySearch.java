c47a1321365fcc80c21a3b91eff7a3ad19a65efb
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-04-25 17:14:47 +0530

    create method recursiveBinary

--- src/binary/BinarySearch.java
+++ src/binary/BinarySearch.java
@@ -1,4 +1,19 @@
 package binary;
 
 public class BinarySearch {
+    public int recursiveBinary(int[] array, int target, int first, int last) {
+        if (first > last)
+            return -1;
+        else {
+            int mid = (first + last) / 2;
+            if (array[mid] == target)
+                return array[mid];
+            else if (array[mid] > target)
+                return recursiveBinary(array, target, first, mid - 1);
+            else if (array[mid] < target)
+                return recursiveBinary(array, target, mid + 1, last);
+
+        }
+        return -1;
+    }
 }
