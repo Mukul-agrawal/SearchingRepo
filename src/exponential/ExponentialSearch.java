57c8229f627ece82b71e4b6e036f4fa1ca1268c8
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-04-26 13:39:38 +0530

    create method exponentialSearch

--- src/exponential/ExponentialSearch.java
+++ src/exponential/ExponentialSearch.java
@@ -1,4 +1,16 @@
 package exponential;
 
+import binary.BinarySearch;
+
+
 public class ExponentialSearch {
+    public int exponentialSearch(int[] array, int target, int length) {
+        if (array[0] == target)
+            return array[0];
+        int i = 1;
+        while (i < length && array[i] <= target) {
+            i = i * 2;
+        }
+        return new BinarySearch().recursiveBinary(array, target, i / 2, Math.min(i, length));
+    }
 }
