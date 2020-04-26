bc83dd0bbf256123f65d97d98fd31a5b48bdf212
Mukul-agrawal <mukul.agrawal_cs18@gla.ac.in>
2020-04-26 12:41:18 +0530

    create interpolationSearch method

--- src/interpolation/InterpolationSearch.java
+++ src/interpolation/InterpolationSearch.java
@@ -1,4 +1,25 @@
 package interpolation;
 
 public class InterpolationSearch {
+    public int interpolationSearch(int[] array, int target) {
+        int low = 0;
+        int high = array.length - 1;
+        while (low <= high && target >= array[low] && target <= array[high]) {
+            if (low == high) {
+                if (array[low] == target)
+                    return array[low];
+                else
+                    return -1;
+            }
+            int pos = low + (((high - low) / (array[high] - array[low])) * (target - array[low]));
+            if (array[pos] == target)
+                return array[pos];
+            if (array[pos] < target)
+                low = pos + 1;
+            else
+                high = pos - 1;
+        }
+        return -1;
+    }
+
 }
